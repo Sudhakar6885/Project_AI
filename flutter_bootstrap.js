6a339ad88c5c4357f3f1a10c1be9fad901ed29e0
Sudhakar6885 <sudhakarraovenkata@gmali.com>
2024-08-24 11:52:47 +0530

    Deploy Flutter web app

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4003798183"
+    serviceWorkerVersion: "3765950698"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -57,7 +57,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "a774dc4cbaf66b7f096c465b13e5a186
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "f86bec7e69ec38c5273bff50c89db427",
+"flutter_bootstrap.js": "fa56b76ef6d6bbff4434c494fbc93677",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
